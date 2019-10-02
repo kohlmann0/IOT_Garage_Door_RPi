48e2438f759b80b852c2c9aad01f126206b570dd
Michael Kohlmann <kohlmann0@gmail.com>
2019-10-02 11:08:41 -0500

    Update Mock Hardware
    This is the first attempt to create a Mock for the actual hardware when doing off-line development.
    Within the main project it will use conditional imports -- using either the actual AutomationHat, or this MockIO, that implements a fake AutomationHat interface.

--- hardware/MockIo.py
+++ hardware/MockIo.py
@@ -1,38 +1,73 @@
-import time
-import datetime
+# This is an attempt to Mock the hardware when we are not on a Raspberry Pi, and the AutomationHat is not available.
 
-class MockIo(object):
+class automationhat(object):
 
 	def __init__(self, control_id, config):        
-		self.id = control_id
-		self.title = config["title"]
-		self.pin = -1
-		self.last_action = "INIT"
-		self.last_action_timestamp = datetime.datetime.now()
-		self.state = True
+		self.input = dict
+		self.output = dict
+		self.relay = dict
 
-	def On(self):
-		self.state = True
+		self.input[0] = fakeInput()
+		self.input[1] = fakeInput()
+		self.input[2] = fakeInput()
+		
+		self.output[0] = fakeOutput()
+		self.output[1] = fakeOutput()
+		self.output[2] = fakeOutput()
+		
+		self.relay[0] = fakeRelay()
+		self.relay[1] = fakeRelay()
+		self.relay[2] = fakeRelay()
 
-	def Off(self):
+
+
+class fakeInput(object):
+	def __init__(self):        
 		self.state = False
 
-	def Pulse_On(self, time_seconds):
-		self.On()
-		time.sleep(time_seconds)
-		self.Off()
-		self.last_action = "PULSE_ON"
-		self.last_action_timestamp = datetime.datetime.now()
+	def is_on(self):
+		return self.state == True		
 
-	def Pulse_off(self, time_seconds):
-		self.Off()
-		time.sleep(time_seconds)
-		self.On()
-		self.last_action = "PULSE_OFF"
-		self.last_action_timestamp = datetime.datetime.now()
-
-	def IsOn(self): 
-		return self.state == True
-
-	def IsOff(self):
+	def is_off(self):
 		return self.state == False
+		
+	def mockSet(self, value)
+		self.state = value
+		
+
+
+class fakeOutput(object):
+	def __init__(self):        
+		self.state = False
+
+	def is_on(self):
+		return self.state == True		
+
+	def is_off(self):
+		return self.state == False
+
+	def on(self):
+		self.state == True
+		
+	def off(self):
+		self.state == False
+		
+		
+		
+class fakeRelay(object):
+	def __init__(self):        
+		self.state = False
+
+	def is_on(self):
+		return self.state == True		
+
+	def is_off(self):
+		return self.state == False
+
+	def on(self):
+		self.state == True
+		
+	def off(self):
+		self.state == False
+		
+		
